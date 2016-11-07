4b73c48fd75d7878f81d152ad595d95c9eb53a61
seung-gyum.kim <antksk@gmail.com>
2016-11-07 11:31:19 +0900

    [작업 이력 업데이트] - Lambda expressions 업데이트 - Default Method for Interface 마무리

--- src/test/resources/com/github/antksk/java8_training/data/TestData.java
+++ src/test/resources/com/github/antksk/java8_training/data/TestData.java
@@ -1,10 +1,17 @@
-package com.github.com.antksk.java8_training.data;
+package com.github.antksk.java8_training.data;
 
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 모든 메서드들이 리턴 하는 데이터는 불편 객체 임
+ * @author Seung Gyum Kim
+ *
+ */
 public final class TestData {
-  public List<String> inMemoryDataWithStringList(){
+  
+  public List<String> inMemoryDataWithDummyNameList(){
     return Arrays.asList("peter", "anna", "mike", "xenia");
   }
+  
 }
